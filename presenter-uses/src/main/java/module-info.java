b1de8a7f294c1f696e83d411914414102245f488
dominik.mozny <dominik.mozny@adesso.ch>
2017-11-13 16:14:53 +0100

    Added branchZurich to presenter-uses

--- presenter-uses/src/main/java/module-info.java
+++ presenter-uses/src/main/java/module-info.java
@@ -4,5 +4,6 @@
 module ch.adesso.module.presenterUses {
     requires ch.adesso.module.branch;
     requires ch.adesso.module.branchBern;
+    requires ch.adesso.module.branchZurich;
     uses ch.adesso.pkg.branch.Branch;
 }
